b81eb486186f4c8d83e12d437b37bb76c25e1d9b
Tommy Kelly <git@tkel.ly>
2016-07-24 01:20:17 -0700

    Begin javascript to fill in the scaffolding

--- index.js
+++ index.js
@@ -7,9 +7,52 @@ const getYouTubeID = require('get-youtube-id')
  * invalid id -> ERROR: PLMC9KNkInc: YouTube said: This video does not exist.
  */
 
-const id = getYouTubeID('http://www.youtube.com/watch?v=9bZkp7q19f0')
-console.log(id)
+function displayDownloadButton() {
+  const downloadAnchor = document.getElementById('download')
+  if (window.getComputedStyle(downloadAnchor).getPropertyValue('display') === 'none') {
+    downloadAnchor.style.display = 'block'
+  }
+}
 
-const downloadAnchor = document.getElementById('download')
-downloadAnchor.href = id
-downloadAnchor.setAttribute('download', 'blah')
+function hideDownloadButton() {
+
+}
+
+function getMetadata(id) {
+  return fetch(`http://localhost:5000/metadata/${id}`)
+  .then(res => {
+    if (!res.ok) {
+      return res.text().then(text => Promise.reject(text))
+    }
+
+    return res.json()
+  })
+}
+
+function genFormat(format) {
+
+}
+
+function displayFormats(formats) {
+  const container = document.createElement('div')
+  for (const format of formats) {
+    genFormat(format)
+  }
+
+  document.getElementById('main').appendChild(container)
+}
+
+function displayDownloadButton(format) {
+  displayDownloadButton()
+  const downloadAnchor = document.getElementById('download')
+  downloadAnchor.href = format.url
+  downloadAnchor.setAttribute('download', 'blah')
+}
+
+
+function initEvents() {
+  document.getElementById('url').addEventListener('change', ev => {
+    const id = getYouTubeID('http://www.youtube.com/watch?v=9bZkp7q19f0')
+    if (id) getMetadata(id).then(displayFormats) 
+  })
+}
